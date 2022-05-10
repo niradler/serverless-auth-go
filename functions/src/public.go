ac6884c65e16ca9d45b4a61aecdb42d305343a85
Nir Adler <niradler@users.noreply.github.com>
2022-05-11 00:01:05 +0300

    router

--- functions/src/public.go
+++ functions/src/public.go
@@ -15,52 +15,75 @@ import (
 
 var ginLambda *ginadapter.GinLambda
 
-func LoadRoutes(r *gin.Engine) {
+func LoadRoutes(router *gin.Engine) {
 
-	r.POST("/public/login", func(context *gin.Context) {
-		type Body struct {
-			Email    string `json:"email" binding:"required"`
-			Password string `json:"password" binding:"required"`
-		}
-		body := Body{}
-		err := context.ShouldBindJSON(&body)
-		if err != nil {
-			context.AbortWithStatusJSON(http.StatusBadRequest,
-				gin.H{
-					"error":   "ValidationError",
-					"message": err.Error(),
-				})
-			return
-		}
+	public := router.Group("/public")
+	{
+		public.POST("/login", func(context *gin.Context) {
+			type Body struct {
+				Email    string `json:"email" binding:"required"`
+				Password string `json:"password" binding:"required"`
+			}
+			body := Body{}
+			err := context.ShouldBindJSON(&body)
+			if err != nil {
+				context.AbortWithStatusJSON(http.StatusBadRequest,
+					gin.H{
+						"error":   "ValidationError",
+						"message": err.Error(),
+					})
+				return
+			}
 
-		context.JSON(http.StatusOK, gin.H{
-			"message": "Login success",
+			context.JSON(http.StatusOK, gin.H{
+				"message": "Login success",
+			})
 		})
-	})
 
-	r.POST("/public/signup", func(context *gin.Context) {
-		type Body struct {
-			Email    uint `json:"email" binding:"required"`
-			Password uint `json:"password" binding:"required"`
-		}
-		body := Body{}
-		err := context.ShouldBindJSON(&body)
-		if err != nil {
-			log.Printf("fmt")
-			fmt.Println(err)
-			context.AbortWithStatusJSON(http.StatusBadRequest,
-				gin.H{
-					"error":   "ValidationError",
-					"message": err.Error(),
-				})
-			return
-		}
+		public.POST("/login", func(context *gin.Context) {
+			type Body struct {
+				Email    string `json:"email" binding:"required"`
+				Password string `json:"password" binding:"required"`
+			}
+			body := Body{}
+			err := context.ShouldBindJSON(&body)
+			if err != nil {
+				context.AbortWithStatusJSON(http.StatusBadRequest,
+					gin.H{
+						"error":   "ValidationError",
+						"message": err.Error(),
+					})
+				return
+			}
 
-		context.JSON(http.StatusOK, gin.H{
-			"message": "Signup success",
+			context.JSON(http.StatusOK, gin.H{
+				"message": "Login success",
+			})
 		})
-	})
 
+		public.POST("/signup", func(context *gin.Context) {
+			type Body struct {
+				Email    uint `json:"email" binding:"required"`
+				Password uint `json:"password" binding:"required"`
+			}
+			body := Body{}
+			err := context.ShouldBindJSON(&body)
+			if err != nil {
+				log.Printf("fmt")
+				fmt.Println(err)
+				context.AbortWithStatusJSON(http.StatusBadRequest,
+					gin.H{
+						"error":   "ValidationError",
+						"message": err.Error(),
+					})
+				return
+			}
+
+			context.JSON(http.StatusOK, gin.H{
+				"message": "Signup success",
+			})
+		})
+	}
 }
 
 func Handler(
@@ -72,15 +95,15 @@ func Handler(
 
 func main() {
 	log.Printf("Gin cold start")
-	r := gin.Default()
-	r.Use(gin.Logger())
-	LoadRoutes(r)
+	router := gin.Default()
+	router.Use(gin.Logger())
+	LoadRoutes(router)
 
-	ginLambda = ginadapter.New(r)
+	ginLambda = ginadapter.New(router)
 
 	if os.Getenv("LAMBDA_TASK_ROOT") != "" {
 		lambda.Start(Handler)
 		return
 	}
-	r.Run("localhost:8282")
+	router.Run("localhost:8282")
 }
